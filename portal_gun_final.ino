8bb489bbbafb710d1dda1146a1d3906ac7aee6ae
Paige Maute <paige.maute@gmail.com>
2023-09-15 23:48:15 +0000

    refactor: change code to fit schematic with 2 switches 1 button

--- portal_gun_final.ino
+++ portal_gun_final.ino
@@ -9,19 +9,21 @@ TMRpcm tmrpcm;
 
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN     6
-#define NUM_LEDS    200
+#define NUM_LEDS    30
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define DUO_TOGGLE A1
-#define BLU_BUTT A2
-#define ORG_BUTT A3
+#define DUO_TOGGLE A2
+#define COL_TOGGLE A3
+#define BUTTON A4
 bool duo_line = true;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ORG_BUTT, INPUT);
-  pinMode(BLU_BUTT, INPUT); 
+  pinMode(BUTTON, INPUT);
+  pinMode(COL_TOGGLE, INPUT); 
   pinMode(DUO_TOGGLE, INPUT);
+  strip.begin();
+  strip.clear();
 
   tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
   tmrpcm.volume(5);
@@ -34,15 +36,17 @@ void setup() {
   Serial.println("SD Connected.");
 
 
-  strip.begin();
-  strip.setBrightness(40);
-  strip.clear();
 
   tmrpcm.play("PWR.wav");
   delay(500);
 
+  int j = NUM_LEDS;
   for (int i = 0; i <= NUM_LEDS; i++) {
-    strip.setPixelColor(i, 0,36,135);
+    strip.setPixelColor(i, 10,30,255);
+    strip.show();
+    delay(15);
+    strip.setPixelColor(j, 10,30,255);
+    j--;
     strip.show();
     delay(15);
   }
@@ -55,57 +59,60 @@ void setup() {
 }
 
 void shoot_blue(){
-  strip.Color(0,36,135);
+  setColor(10,30,255);
   strip.show();
   tmrpcm.play("BLU.wav");
   delay(1200);
 }
 
 void shoot_orange(){
-  strip.Color(85,13,0);
+  setColor(255,40,0);
   strip.show();
   tmrpcm.play("ORG.wav");
   delay(1200);
 }
 
 void error() {
-  strip.Color(0,9,31);
+  setColor(0,9,31);
   strip.show();
   tmrpcm.play("FAL.wav");
   delay(1000);
-  strip.Color(0,36,135);
+  setColor(10,30,255);
   strip.show();
+}
+
+void setColor(int r, int g, int b) {
+  for (int i = 0; i <= NUM_LEDS; i++) {
+    strip.setPixelColor(i, r, g, b);
+  }
+  strip.show();
+}
+
+void wait() {
+  while(digitalRead(BUTTON) == LOW){
+    if(duo_line && digitalRead(DUO_TOGGLE) == HIGH){
+      tmrpcm.play("DOS.wav");
+      delay(5500);
+      duo_line = false;
+    } else {
+      setColor(10,30,255);
+      strip.show();
+    }
+  }
 }
 
 
 void loop(){
 
-  if(digitalRead(DUO_TOGGLE) == LOW) {
-    strip.Color(0,36,135);
-    strip.show();
+  wait();
 
-    if(digitalRead(BLU_BUTT) == HIGH) {
-      shoot_blue();
-    }
-
-    if(digitalRead(ORG_BUTT) == HIGH) {
+  if(digitalRead(COL_TOGGLE) == LOW) {
+    shoot_blue();
+  } else {
+    if(digitalRead(DUO_TOGGLE) == LOW) {
       error();
-    }
-  }
-
-  if(digitalRead(DUO_TOGGLE) == HIGH) {
-    if(duo_line) {
-      tmrpcm.play("DOS.wav");
-      delay(5500);
-      duo_line = false;
-    }
-
-    if(digitalRead(ORG_BUTT) == HIGH) {
+    } else {
       shoot_orange();
-    }
-
-    if(digitalRead(BLU_BUTT) == HIGH) {
-      shoot_blue();
     }
   }
 }
